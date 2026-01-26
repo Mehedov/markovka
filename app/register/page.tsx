0d409a82f1f4ed8dc9184fa0e63cdb055c4b8965
Mehedov <mehedov.dev@yandex.ru>
2026-01-26 18:14:40 +0300

    ui: rename placeholder

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -26,7 +26,7 @@ export default function LoginPage() {
       <Card title="Вход для администратора" style={{ width: 400 }}>
         <Form onFinish={onFinish} layout="vertical">
           <Form.Item name="email" label="Email" rules={[{ required: true, type: 'email' }]}>
-            <Input placeholder="admin@example.com" />
+            <Input placeholder="test@example.com" />
           </Form.Item>
           <Form.Item name="password" label="Пароль" rules={[{ required: true }]}>
             <Input.Password />
